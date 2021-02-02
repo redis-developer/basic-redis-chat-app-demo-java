0bb52333f7cc6b26b19593ae5b63447f9b42b19d
Oleg Pustovit <oleg.pustovit@dinamicka.com>
2021-02-02 11:05:40 +0200

    Add port from env

--- src/main/java/com/redisdeveloper/basicchat/Application.java
+++ src/main/java/com/redisdeveloper/basicchat/Application.java
@@ -1,11 +1,21 @@
 package com.redisdeveloper.basicchat;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        // Read environment variables
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "8080";
+        }
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        app.run(args);
+
     }
 }

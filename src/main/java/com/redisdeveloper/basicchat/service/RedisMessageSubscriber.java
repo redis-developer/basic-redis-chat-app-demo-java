babb361c92048a4b4b0f4f9a8442c6c59a4f7113
yurii_lampak <y.lampak@dinamicka.com>
2021-02-12 19:31:50 +0200

    update subscriber

--- src/main/java/com/redisdeveloper/basicchat/service/RedisMessageSubscriber.java
+++ src/main/java/com/redisdeveloper/basicchat/service/RedisMessageSubscriber.java
@@ -1,15 +1,11 @@
 package com.redisdeveloper.basicchat.service;
 
-import com.redisdeveloper.basicchat.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Function;
 
@@ -17,23 +13,13 @@ import java.util.function.Function;
 public class RedisMessageSubscriber implements MessageListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisMessageSubscriber.class);
 
-    @Autowired
-    private StringRedisTemplate redisTemplate;
-
     CopyOnWriteArrayList<Function<String, Integer>> handlers = new CopyOnWriteArrayList<>();
-
-    @PostConstruct
-    void init() {
-        try {
-            Utils.createDemoData(redisTemplate);
-        } catch (Exception e) {
-            LOGGER.error("Error occurred when creating demo data", e);
-        }
-    }
 
     @Override
     public void onMessage(final Message message, final byte[] pattern) {
         String messageBody = new String(message.getBody());
+
+        LOGGER.debug("Received message in global subscriber: "+ message.toString());
 
         for (Function<String, Integer> handler : handlers) {
             handler.apply(messageBody);
